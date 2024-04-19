3dbc687e24406e57f600f42bb2e20ddee9b4309b
CrazyCows <emil.s.simonsen@gmail.com>
2024-04-19 17:12:31 +0200

    made endpoints good

---
+++ helpers/password_encrypt.py
@@ -0,0 +1,17 @@
+import bcrypt
+
+class Encryption:
+    def hash_password(self, password):
+        password_bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password_bytes, salt)
+        return hashed, salt
+
+    def verify_password(self, stored_hash, password: str):
+        password_bytes = password.encode('utf-8')
+        if bcrypt.checkpw(password_bytes, stored_hash):
+            return True
+        else:
+            return False
+
+
