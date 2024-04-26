4ba6ba6ec078c9f0bccbb255e74004c6eb72e7a0
Anders Nørgaard <ansi1806@gmail.com>
2024-04-26 13:13:33 +0200

    converted all sql commands to peewee sql :3

--- src/database/models.py
+++ src/database/models.py
@@ -4,12 +4,33 @@ from peewee import *
 import uuid
 
 # Database connection
-db = PostgresqlDatabase('my_database.db')
+db = PostgresqlDatabase('backend_school', user='firstuser', password='Studyhard1234.',
+                        host='localhost', port=5432)
+
+get_shift_for_month_function_query = function_query = """
+    CREATE OR REPLACE FUNCTION get_shifts_for_month(month_date DATE, users_id INTEGER) RETURNS TABLE (
+       shift_id INT,
+       start_time TIMESTAMP,
+       end_time TIMESTAMP,
+       active BOOLEAN,
+       myShift BOOLEAN
+    ) AS $$
+    BEGIN
+       RETURN QUERY
+       SELECT shifts.shift_id, shifts.start_time, shifts.end_time, shifts.active,
+       CASE WHEN shift_member.user_id = users_id THEN TRUE ELSE FALSE END AS myShift
+       FROM shifts JOIN shift_member ON shifts.shift_id = shift_member.shift_id
+       WHERE EXTRACT(YEAR FROM shifts.start_time) = EXTRACT(YEAR FROM month_date)
+         AND EXTRACT(MONTH FROM shifts.start_time) = EXTRACT(MONTH FROM month_date);
+    END;
+    $$ LANGUAGE plpgsql;
+"""
 
 # Define your base model class from which all tables will inherit
 class BaseModel(Model):
     class Meta:
         database = db
+
 
 class clearence_lvl(BaseModel):
     id = AutoField(primary_key=True)
@@ -17,10 +38,11 @@ class clearence_lvl(BaseModel):
     role = CharField(null=False, max_length=100, unique=True)
     creation_date = DateTimeField(datetime.datetime.now())
 
+
 # Define a simple User table
 class users(BaseModel):
     id = AutoField(primary_key=True)
-    uid_user = UUIDField(unique=True) #uuid_generate???
+    uid_user = UUIDField(unique=True)  # uuid_generate???
     name = CharField(max_length=255)
     email = CharField(max_length=255, unique=True)
     phone = CharField(max_length=8, unique=True)
@@ -32,6 +54,7 @@ class users(BaseModel):
     salt = BinaryUUIDField()
     password = BinaryUUIDField()
 
+
 class shifts(BaseModel):
     id = AutoField(primary_key=True)
     uid_shift = UUIDField(unique=True)
@@ -39,7 +62,8 @@ class shifts(BaseModel):
     end_time = DateTimeField()
     active = BooleanField()
     creation_date = DateTimeField(datetime.datetime.now())
-    
+
+
 class shift_member(BaseModel):
     id = AutoField(primary_key=True)
     uid_shift = ForeignKeyField(shifts, to_field='uid_shift', on_delete='CASCADE')
@@ -47,11 +71,17 @@ class shift_member(BaseModel):
     attendance = BooleanField(default=False)
     wished = BooleanField(default=False)
     assigned = BooleanField(default=False)
+
+
 def create_tables():
     with db:
         db.create_tables([clearence_lvl, users, shifts, shift_member])
 
+def create_function():
+    with db:
+        db.execute_sql(get_shift_for_month_function_query)
+
 # Main execution logic
 if __name__ == '__main__':
-    create_tables()
+    create_function()
     # You can add instances here or perform other database operations
--- src/database/users.py
+++ src/database/users.py
@@ -4,6 +4,8 @@ from src.database.conn_pool import PoolUsersData
 from src.models.users_model import User, UserLogin, CreateUser
 from src.helpers.password_encrypt import Encryption
 from typing import List
+from models import users, db
+from datetime import datetime
 
 # This is just an example to show how to use the pool and the database connection - database does not exist yet
 # Transactions is used for deletes and inserts to prevent the data from being read when modified.
@@ -12,76 +14,62 @@ class Database:
         self.pool = PoolUsersData()
 
     async def create_user(self, user: CreateUser) -> None:
-        hashed_password, salt = Encryption().hash_password(user.password)
-        async with self.pool.acquire() as conn:
-            create_statement = ("INSERT INTO users(name, email, phone, role, username, password, salt) "
-                                "VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT DO NOTHING")
-            params = [user.name, user.email, user.phone, user.role, user.username, hashed_password, salt]
-            await conn.execute(create_statement, *params)
+        async with db.atomic_async():
+            hashed_password, salt = Encryption().hash_password(user.password)
+            await users.get_or_create(
+                uid_user=user.uid_user,
+                name=user.name,
+                email=user.email,
+                phone=user.phone,
+                role=user.role,
+                username=user.username,
+                last_login=datetime.now(),
+                registration=datetime.now(),
+                last_modified=datetime.now(),
+                salt=salt,
+                password=hashed_password
+            )
 
     async def update_user(self, user: User) -> None:
-        async with self.pool.acquire() as conn:
-            update_statement = "UPDATE users SET name = $2, email = $3, phone = $4, role = $5 WHERE uid_user=$1"
-            params = [user.uid_user, user.name, user.email, user.phone, user.role]
-            await conn.execute(update_statement, *params)
+        async with db.atomic_async():
+            await users.update(
+                name=user.name,
+                email=user.email,
+                phone=user.phone,
+                role=user.role
+            ).where(users.uid_user == user.uid_user).execute()
 
     async def delete_user(self, user: User) -> None:
-        async with self.pool.acquire() as conn:
-            delete_statement = "DELETE FROM users WHERE users.uid_user = $1"
-            params = user.uid_user
-            await conn.execute(delete_statement, params)
+        async with db.atomic_async():
+            await users.delete().where(users.uid_user == user.uid_user).execute()
 
     async def fetch_all_users(self) -> List[User]:
-        async with self.pool.acquire() as conn:
-            query = "SELECT users.uid_user, users.name, users.email, users.phone, users.role FROM users"
-            result = await conn.execute(query)
-            users = [User(uid_user=user.get('uid_user'),
-                          user_name=user.get('name'),
-                          email=user.get('email'),
-                          phone=result.get('phone'),
-                          role=user.get('role')) for user in result]
-            return users
+        async with db.atomic_async():
+            all_users = await users.select()
+            return [user for user in all_users]
 
     async def fetch_user(self, user: UserLogin, active_user: User = None) -> User:
-        async with self.pool.acquire() as conn:
+        async with db.atomic_async():
+            user_query = (users
+                          .select()
+                          .where((users.username == user.username) | (users.uid_user == user.uid_user)))
 
-            query = "SELECT users.password, users.uid_user, users.name, users.email, users.phone, users.role FROM users WHERE users.username=$1 or users.uid_user=$2"
-            params = [user.username, active_user.uid_user if active_user else None]
-            result = await conn.fetchrow(query, *params)
-
-            # TODO: Refractor this, password check does not fit here
             if active_user is None:
-                correct_password = Encryption().verify_password(result.get('password'), user.password)
-                if correct_password is False:
-                    raise Exception("false password")
-            print(result)
-            print(result.get('uid_user'))
-
-            user = User(uid_user=str(result.get('uid_user')),
-                        name=result.get('name'),
-                        email=result.get('email'),
-                        phone=result.get('phone'),
-                        role=result.get('role'))
-            return user
+                user = await user_query.get()
+                correct_password = Encryption().verify_password(user.password, user.password)
+                if not correct_password:
+                    raise Exception("Incorrect password")
+                return user
+            else:
+                user_query = user_query.where(users.uid_user == active_user.uid_user)
+                return await user_query.get()
 
     async def fetch_user_by_id(self, user: User) -> User:
-        async with self.pool.acquire() as conn:
-
-            query = "SELECT users.uid_user, users.name, users.email, users.phone, users.role FROM users WHERE users.uid_user=$1 AND users.active=TRUE"
-            params = user.uid_user
-            result = await conn.fetchrow(query, params)
-
-            if result is None:
-                raise Exception("user not found")
-
-            user = User(uid_user=str(result.get('uid_user')),
-                        name=result.get('name'),
-                        email=result.get('email'),
-                        phone=result.get('phone'),
-                        role=result.get('role'))
+        async with db.atomic_async():
+            user = await users.get_or_none(users.uid_user == user.uid_user)
+            if not user:
+                raise Exception("User not found")
             return user
-
-
 
 async def main():
     base_pool = await PoolUsersData().initialize_pool()
